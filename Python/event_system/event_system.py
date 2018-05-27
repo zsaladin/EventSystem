07683c01d69765a6e7993795b41bf7c8178caf7a
daehee.kim <zsaladinz@gmail.com>
2018-05-28 02:11:08 +0900

    Add order for Python

--- Python/event_system/event_system.py
+++ Python/event_system/event_system.py
@@ -1,16 +1,21 @@
 from collections import defaultdict
 
 
-
 class EventSystem:
+    class Callback:
+        def __init__(self, callback, order):
+            self.callback = callback
+            self.order = order
+
     def __init__(self):
         self.__callbacks = defaultdict(list)
 
-    def subscribe(self, event, callback:callable):
-        callbacks = self.__callbacks[event]
-        callbacks.append(callback)
+    def subscribe(self, event, callback, order=0):
+        callbacks: list = self.__callbacks[event]
+        callbacks.append(EventSystem.Callback(callback, order))
+        callbacks.sort(key=lambda c: c.order)
 
     def publish(self, event, *args, **kwargs):
         callbacks = self.__callbacks[event]
         for callback in callbacks:
-            callback(*args, **kwargs)
+            callback.callback(*args, **kwargs)
--- Python/main.py
+++ Python/main.py
@@ -1,16 +1,28 @@
 from event_system import Event, EventSystem
 
-def callback_create_tx(tx_hash):
-    print(tx_hash)
+def callback_create_tx0(tx_hash):
+    print(f"{tx_hash}, order 0")
 
 
-def callback_create_block(tx_block):
-    print(tx_block)
+def callback_create_tx10(tx_hash):
+    print(f"{tx_hash}, order 10")
+
+
+def callback_create_block0(tx_block):
+    print(f"{tx_block}, order 0")
+
+
+def callback_create_block10(tx_block):
+    print(f"{tx_block}, order 10")
 
 
 event_system = EventSystem()
-event_system.subscribe(Event.create_tx, callback_create_tx)
-event_system.subscribe(Event.create_block, callback_create_block)
+
+event_system.subscribe(Event.create_tx, callback_create_tx10, 10)
+event_system.subscribe(Event.create_tx, callback_create_tx0)
+
+event_system.subscribe(Event.create_block, callback_create_block0)
+event_system.subscribe(Event.create_block, callback_create_block10, 10)
 
 event_system.publish(Event.create_tx, "123")
 event_system.publish(Event.create_block, tx_block="abc")
