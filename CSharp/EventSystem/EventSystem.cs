275ad28769325a65da397dad8e0ccacd75e897df
daehee.kim <zsaladinz@gmail.com>
2018-05-28 02:03:41 +0900

    Add order for C#

--- CSharp/EventSystem/EventSystem.cs
+++ CSharp/EventSystem/EventSystem.cs
@@ -5,23 +5,34 @@ namespace EventSystem
 {
     class EventSystem
     {
-        private Dictionary<Type, List<MulticastDelegate>> _callbacks = new Dictionary<Type, List<MulticastDelegate>>();
-
-        public void Subscribe<TEvent>(Action<TEvent> callback) where TEvent : Event
+        class Callback
         {
-            if (_callbacks.TryGetValue(typeof(TEvent), out List<MulticastDelegate> callbacks) == false)
+            public MulticastDelegate Delegate { get; set; }
+            public int Order { get; set; }
+        }
+
+        private Dictionary<Type, List<Callback>> _callbacks = new Dictionary<Type, List<Callback>>();
+
+        public void Subscribe<TEvent>(Action<TEvent> callback, int order = 0) where TEvent : Event
+        {
+            if (_callbacks.TryGetValue(typeof(TEvent), out List<Callback> callbacks) == false)
             {
-                callbacks = new List<MulticastDelegate>();
+                callbacks = new List<Callback>();
                 _callbacks.Add(typeof(TEvent), callbacks);
             }
-            callbacks.Add(callback);
+            callbacks.Add(new Callback()
+            {
+                Delegate = callback,
+                Order = order
+            });
+            callbacks.Sort((x, y) => x.Order - y.Order);
         }
 
         public void Publish<TEvent>(TEvent e) where TEvent : Event
         {
-            if (_callbacks.TryGetValue(typeof(TEvent), out List<MulticastDelegate> callbacks))
+            if (_callbacks.TryGetValue(typeof(TEvent), out List<Callback> callbacks))
             {
-                callbacks.ForEach(callback => ((Action<TEvent>)callback)(e));
+                callbacks.ForEach(callback => ((Action<TEvent>)callback.Delegate)(e));
             }
         }
     }
--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -7,27 +7,41 @@ namespace EventSystem
         static void Main(string[] args)
         {
             EventSystem eventSystem = new EventSystem();
-            eventSystem.Subscribe<Event_CreateBlock>(callback);
-            eventSystem.Subscribe<Event_CreateTransaction>(callback);
+
+            eventSystem.Subscribe<Event_CreateBlock>(Callback10, 10);
+            eventSystem.Subscribe<Event_CreateBlock>(Callback0);
+
+            eventSystem.Subscribe<Event_CreateTransaction>(Callback0);
+            eventSystem.Subscribe<Event_CreateTransaction>(Callback10, 10);
 
             Event_CreateBlock event0 = new Event_CreateBlock();
             event0.BlockHash = "abc";
 
             Event_CreateTransaction event1 = new Event_CreateTransaction();
-            event1.TxHash = "bcd";
+            event1.TxHash = "123";
 
             eventSystem.Publish(event0);
             eventSystem.Publish(event1);
         }
 
-        static void callback(Event_CreateBlock e)
+        static void Callback0(Event_CreateBlock e)
         {
-            Console.WriteLine(e.BlockHash);
+            Console.WriteLine($"Order 0, {e.BlockHash}");
         }
 
-        static void callback(Event_CreateTransaction e)
+        static void Callback10(Event_CreateBlock e)
         {
-            Console.WriteLine(e.TxHash);
+            Console.WriteLine($"Order 10, {e.BlockHash}");
+        }
+
+        static void Callback0(Event_CreateTransaction e)
+        {
+            Console.WriteLine($"Order 0, {e.TxHash}");
+        }
+
+        static void Callback10(Event_CreateTransaction e)
+        {
+            Console.WriteLine($"Order 10, {e.TxHash}");
         }
     }
 }
